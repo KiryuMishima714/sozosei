7ca9d1535e557ea6979523adca3ceda4ef77102f
KiryuMishima714 <kiryu1357910@gmail.com>
2023-11-24 15:23:11 +0900

    chat comand

--- raspi/juliu.py
+++ raspi/juliu.py
@@ -1,26 +1,18 @@
 import subprocess
 
-def run_julius(julius_path, jconf_path):
-    julius_command = f"{julius_path} -C {jconf_path}"
-    julius_process = subprocess.Popen(julius_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# Juliusコマンドの実行
+command = ['julius', '-C', 'main.jconf', '-C', 'am-gmm.jconf', '-demo']
 
-    try:
-        while True:
-            output = julius_process.stdout.readline().strip()
+# subprocessモジュールを使用してコマンドを実行
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-            # 認識結果が出力されたら表示
-            if "sentence1:" in output:
-                recognized_words = output.split("sentence1:")[1].strip()
-                print("Recognized words:", recognized_words)
+# 出力を表示
+stdout, stderr = process.communicate()
 
-    except KeyboardInterrupt:
-        # プログラムが中断されたらプロセスを終了
-        julius_process.terminate()
+print("Standard Output:")
+print(stdout)
 
-if __name__ == "__main__":
-    # Juliusの実行ファイルと設定ファイルのパスを指定
-    julius_path = "/path/to/julius"
-    jconf_path = "/path/to/your/julius.jconf"
+if stderr:
+    print("Error Output:")
+    print(stderr)
 
-    # Juliusの実行
-    run_julius(julius_path, jconf_path)
